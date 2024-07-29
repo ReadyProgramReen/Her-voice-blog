9198d8f1f2c023aaaf4d9a35041d01a85e9b7bdb
Maureen E <maureenekeugo1@gmail.com>
2024-07-29 16:03:07 -0400

    Init project

---
+++ Components/Header.jsx
@@ -0,0 +1,16 @@
+import { assets } from '@/Assets/Assets/assets'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+        <div className='flex justify-between item-center'>
+            <h1 className='text-3xl font-semibold text-sky-800 md:text-4xl lg:text-5xl'>"Her Voice"</h1>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
