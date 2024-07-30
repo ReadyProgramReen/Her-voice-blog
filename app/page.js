e66f047567ad7913a6d2a5a833012a8271a701a7
Maureen E <maureenekeugo1@gmail.com>
2024-07-29 21:38:46 -0400

    Used map to display blog item componet from the blog database

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 'Use client'
 import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
     <Header/>
-    <BlogItem/>
+    <BlogList/>
+   
     </>
     
   );
