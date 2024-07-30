b70f2f540f36455e098d129133d52a150b4e994f
Maureen E <maureenekeugo1@gmail.com>
2024-07-29 21:23:43 -0400

    Completed blog item component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+'Use client'
+import BlogItem from "@/Components/BlogItem";
 import Header from "@/Components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <BlogItem/>
     </>
     
   );
